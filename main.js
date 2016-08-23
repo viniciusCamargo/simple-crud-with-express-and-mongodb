588d28ab57391893be86a77fa907b14168f05fc7
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-08-23 00:37:58 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+app.use(express.static('public'));
+
+var update = document.getElementById('update');
+
+update.addEventListener('click', function() {
+
+});
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+var db;
+// MongoClient.connect('mongodb://root:admin@ds013946.mlab.com:13946/star-wars-quotes', (err, database) => {
+MongoClient.connect('mongodb://localhost:27017/star-wars-quotes', (err, database) => {
+  if (err) return console.log(err);
+
+  db = database;
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  });
+});
+
+// app.listen(3000, function() {
+//   console.log('listening on 3000');
+// });
+
+// app.get('/', function(req, res) {
+//   res.send('Hello World');
+// });
+
+// app.get('/form', (req, res) => {
+//   res.sendFile('/home/vinicius/Documents/code/express' + '/index.html');
+// });
+
+app.get('/', (req, res) => {
+  // var cursor = db.collection('quotes').find().toArray(function(err, results) {
+  db.collection('quotes').find().toArray(function(err, result) {
+    if (err) return console.log(err);
+
+    res.render('index.ejs', {quotes: result});
+  });
+
+  // console.log(cursor);
+});
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/');
+  });
+});
