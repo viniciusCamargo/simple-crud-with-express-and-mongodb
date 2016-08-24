84b88902dcae275cb8f06347961ad981a0f57115
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-08-23 21:08:08 -0300

    Finaliza 'put'

--- public/main.js
+++ public/main.js
@@ -1,7 +1,18 @@
-app.use(express.static('public'));
+var update = document.getElementById('update')
 
-var update = document.getElementById('update');
-
-update.addEventListener('click', function() {
-
-});
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Darth Vader',
+      'quote': 'I find your lack of faith disturbing.'
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+})
--- server.js
+++ server.js
@@ -1,52 +1,72 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-var db;
+var db
 // MongoClient.connect('mongodb://root:admin@ds013946.mlab.com:13946/star-wars-quotes', (err, database) => {
 MongoClient.connect('mongodb://localhost:27017/star-wars-quotes', (err, database) => {
-  if (err) return console.log(err);
+  if (err) return console.log(err)
 
-  db = database;
+  db = database
   app.listen(3000, () => {
-    console.log('listening on 3000');
-  });
-});
+    console.log('listening on 3000')
+  })
+})
 
 // app.listen(3000, function() {
-//   console.log('listening on 3000');
-// });
+//   console.log('listening on 3000')
+// })
 
 // app.get('/', function(req, res) {
-//   res.send('Hello World');
-// });
+//   res.send('Hello World')
+// })
 
 // app.get('/form', (req, res) => {
-//   res.sendFile('/home/vinicius/Documents/code/express' + '/index.html');
-// });
+//   res.sendFile('/home/vinicius/Documents/code/express' + '/index.html')
+// })
 
 app.get('/', (req, res) => {
   // var cursor = db.collection('quotes').find().toArray(function(err, results) {
   db.collection('quotes').find().toArray(function(err, result) {
-    if (err) return console.log(err);
+    if (err) return console.log(err)
 
-    res.render('index.ejs', {quotes: result});
-  });
+    res.render('index.ejs', {quotes: result})
+  })
 
-  // console.log(cursor);
-});
+  // console.log(cursor)
+})
 
 app.post('/quotes', (req, res) => {
   db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err);
+    if (err) return console.log(err)
 
-    console.log('saved to database');
-    res.redirect('/');
-  });
-});
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+    .findOneAndUpdate({
+      name: 'Yoda'
+    }, {
+      $set: {
+        name: req.body.name,
+        quote: req.body.quote
+      }
+    }, {
+      sort: {_id: -1},
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+
+      res.send(result)
+    })
+})
